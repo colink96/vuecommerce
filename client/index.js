d8365e29329572386de26fd96c9dd815644ce2a5
Colin Kim <ck2344@nyu.edu>
2020-03-18 16:49:06 -0400

    listed all products from our db

--- client/index.js
+++ client/index.js
@@ -1,11 +1,30 @@
 import './socket'
 import Vue from 'vue'
+import axios from 'axios'
 
 Vue.component('app-head', {props: ['text'], template: '<h1>{{text}}</h1>'})
+
+Vue.component('all-products', {
+  props: ['products', 'fetched'],
+  template:
+    '<div v-bind:products="products"><ol><single-product v-for="product in products" :key="product.id" :product="product"></single-product></ol></div>'
+})
+
+Vue.component('single-product', {
+  props: ['product'],
+  template: '<li>{{product.name}}</li>'
+})
 
 var app = new Vue({
   el: '#app',
   data: {
+    products: [],
     header: 'Welcome to VueCommerce!'
+  },
+  async mounted() {
+    const {data} = await axios.get('/api/products')
+    this.products = data
+    this.fetched = true
+    console.log(data)
   }
 })
